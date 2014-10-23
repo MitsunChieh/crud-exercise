1120596816ad09844cb9f91b17d432c5f3304789
MitsunChieh <Mitsun.Chieh@gmail.com>
2014-10-24 03:08:06 +0800

    Add new and create. fixed class variable dramas to drama.

--- app/controllers/japan_dramas_controller.rb
+++ app/controllers/japan_dramas_controller.rb
@@ -1,5 +1,22 @@
 class JapanDramasController < ApplicationController
   def index
-    @dramas = JapanDrama.page(params[:page]).per(10)
+    @drama = JapanDrama.page(params[:page]).per(10)
+  end
+
+  def new
+    @drama = JapanDrama.new
+  end
+
+  def create
+    @drama = JapanDrama.new(drama_params)
+    @drama.save
+
+    redirect_to japan_dramas_url
+  end
+
+  private
+
+  def drama_params
+    params.require(:japan_drama).permit(:name, :date, :actor)
   end
 end
