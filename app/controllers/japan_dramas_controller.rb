764eb3abf99543fc4b732809abde14b6ae640226
MitsunChieh <Mitsun.Chieh@gmail.com>
2014-10-24 03:40:18 +0800

    Add Read, Upgrate, andestory. Practicing befor_action, and fixed some problem with the deletee url

--- app/controllers/japan_dramas_controller.rb
+++ app/controllers/japan_dramas_controller.rb
@@ -1,4 +1,7 @@
 class JapanDramasController < ApplicationController
+
+  before_action :set_drama, :only => [:show, :edit ,:update, :destroy]
+
   def index
     @drama = JapanDrama.page(params[:page]).per(10)
   end
@@ -14,9 +17,30 @@ class JapanDramasController < ApplicationController
     redirect_to japan_dramas_url
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @drama.update_attributes(drama_params)
+
+    redirect_to japan_drama_url(@drama)
+  end
+
+  def destory
+    @drama.destroy
+
+    redirect_to events_url
   private
 
   def drama_params
     params.require(:japan_drama).permit(:name, :date, :actor)
   end
+
+  def set_drama
+    @drama = JapanDrama.find(params[:id])
+  end
+
 end
